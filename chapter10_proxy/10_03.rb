c7126f0f647e3012de046157dd7a90eac5380d38
tk0358 <tk0358a@yahoo.co.jp>
2019-02-25 23:52:36 +0900

    10-4 Proxy

---
+++ chapter10_proxy/10_03.rb
@@ -0,0 +1,6 @@
+require 'soap/wsdlDriver'
+
+wsdl_url = 'http://wwww.webservicex.net/WeatherForecast.asmx?WSDL'
+
+proxy = SOAP::WSDLDriverFactory.new( wsdl_url ).create_rpc_driver
+weather_info = proxy.GetWeatherByZipCode('ZipCode' => '19128')
---
+++ chapter10_proxy/10_05.rb
@@ -0,0 +1,44 @@
+# 仮想プロキシーの実装（バージョン２）
+class BankAccount
+  attr_reader :balance
+
+  def initialize(starting_balance=0)
+    @balance = starting_balance
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
+end
+
+class VirtualAccountProxy
+  def initialize(&creation_block)
+    @creation_block = creation_block
+  end
+
+  def deposit(amount)
+    s = subject
+    return s.deposit(amount)
+  end
+
+  def withdraw(amount)
+    s = subject
+    return s.withdraw(amount)
+  end
+
+  def balance
+    s = subject
+    return s.balance
+  end
+
+  def subject
+    @subject || (@subject = @creation_block.call)
+  end
+end
+
+account = VirtualAccountProxy.new { BankAccount.new(10) }
+puts account.balance
